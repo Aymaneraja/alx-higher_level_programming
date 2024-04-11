c8c71c598c37fe09109c9e441f25e1948df61902
Aymaneraja <aymanehs11@gmail.com>
2024-04-11 08:33:18 -0700

    k

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 /* prints the first argument */
-const lenght = process.argv.lenght ;
-if (process.argv[2] == undefined) {
+if (process.argv[2] === undefined) {
 	console.log('No argument');
 } else {
 	console.log(process.argv[2]);
